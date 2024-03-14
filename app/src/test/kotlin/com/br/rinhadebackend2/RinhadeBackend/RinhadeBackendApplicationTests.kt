f9384c7ba8e6e17b48978ce6f5f4c9be736577aa
Guilherme Mello <guilhermecfmello@gmail.com>
2024-03-14 10:02:12 -0300

    First container config

--- app/src/test/kotlin/com/br/rinhadebackend2/RinhadeBackend/RinhadeBackendApplicationTests.kt
+++ app/src/test/kotlin/com/br/rinhadebackend2/RinhadeBackend/RinhadeBackendApplicationTests.kt
@@ -1,13 +1,16 @@
 package com.br.rinhadebackend2.RinhadeBackend
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@Disabled
 class RinhadeBackendApplicationTests {
 
 	@Test
 	fun contextLoads() {
+
 	}
 
 }
