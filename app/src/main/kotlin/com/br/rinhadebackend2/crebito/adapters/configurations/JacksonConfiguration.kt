dd9b6fe9096aa8b02271ddc03c99f2cf5ece99ec
Guilherme Mello <guilhermecfmello@gmail.com>
2024-03-14 12:13:11 -0300

    Fix jackson datetime module dependency.

--- app/src/main/kotlin/com/br/rinhadebackend2/crebito/adapters/configurations/JacksonConfiguration.kt
+++ app/src/main/kotlin/com/br/rinhadebackend2/crebito/adapters/configurations/JacksonConfiguration.kt
@@ -2,13 +2,24 @@ package com.br.rinhadebackend2.crebito.adapters.configurations
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.time.format.DateTimeFormatter
+
 
 @Configuration
 class JacksonConfiguration {
 
     @Bean
-    fun objectMapper(): ObjectMapper = ObjectMapper().configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, false).registerModules(KotlinModule.Builder().build())
+    fun objectMapper(): ObjectMapper = ObjectMapper()
+        .configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, false)
+        .registerModules(KotlinModule.Builder().build())
+        .registerModule(
+            JavaTimeModule()
+                .addSerializer(LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME)
+                )
+        )
 }
